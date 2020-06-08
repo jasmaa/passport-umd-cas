1cfc3b47da3515ecef501c1f33433173addca9a0
jasmaa <jasonmaa3955@gmail.com>
2020-06-08 14:41:12 -0400

    Remove unecessary middleware in login example

--- examples/login/server.js
+++ examples/login/server.js
@@ -16,9 +16,6 @@ const app = express();
 
 app.set('view engine', 'pug');
 
-app.use(require('serve-static')(__dirname + '/../../public'));
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
